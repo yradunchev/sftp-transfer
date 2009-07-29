9cd7101a375a12722428fa2a90c00dd5ecbdb5b5
gtoonstra <gtoonstra@d768a6b0-7c34-11de-bb5b-0fd25fbaf517>
2009-07-29 11:46:17 +0000

    scripts.

---
+++ trunk/send-scripts/response_send.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+# import generic functions for receipt and send processing
+source ./response_gen_functions.sh
+
+# ========================================================
+# Change the following configuration according to your needs
+# ========================================================
+USER=response
+HOST=localhost
+LOCALDIR=/home/virtual/scripts
+REMOTEDIR=inbox
+LOGFILE=`date '+%Y%m%d_%H%M%S'`
+
+# ========================================================
+# DO NOT CHANGE anything below this line
+# ========================================================
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OUTBOX=$LOCALDIR/outbox
+LOGDIR=$LOCALDIR/log
+PROCDIR=$LOCALDIR/processing
+CONTROLDIR=$LOCALDIR/control
+ERRORDIR=$LOCALDIR/error
+ARCHIVEDIR=$LOCALDIR/archive
+
+exec > $LOGDIR/$LOGFILE 2>&1
+
+# ========================================================
+# PROGRAM start
+# ========================================================
+
+START=`date '+%H:%M:%S'`
+
+echo "Starting script @ $START"
+echo "Processing files in $OUTBOX"
+
+# empty control directory
+rm -f $LOCALDIR/control/*
+
+# check what is to be sent. Set up control file and command file for sftp
+for file in `find $OUTBOX -type f`
+do
+	FILENAME=`basename "$file"`
+	mv $file $PROCDIR/$FILENAME
+	touch $CONTROLDIR/control_$FILENAME
+	cat > $CONTROLDIR/commands_$FILENAME << UNTIL_THIS_TOKEN
+cd $REMOTEDIR
+put $PROCDIR/$FILENAME
+put $CONTROLDIR/control_$FILENAME
+UNTIL_THIS_TOKEN
+
+done
+
+script_ret_val=0
+
+# Now process each file and ensure it was sent ok.
+for file in `find $PROCDIR -type f`
+do
+	FILENAME=`basename "$file"`
+	sftp -b $CONTROLDIR/commands_$FILENAME $USER@$HOST
+	check_error $? "Failed SFTP operation for $FILENAME"
+
+	ret_val=$?
+	if [ "${ret_val}" -ne "0" ]; then
+		# Something went wrong with the SFTP operation
+		# Move processing file to error dir.
+		echo "moving $PROCDIR/$FILENAME to $ERRORDIR/" 
+		mv "$PROCDIR/$FILENAME" "$ERRORDIR/"
+
+		# Indicate there were errors
+		script_ret_val=1
+	else
+		# File successfully transferred. Move file to archive dir
+		echo "removing $PROCDIR/$FILENAME"
+		mv "$PROCDIR/$FILENAME" "$ARCHIVEDIR/"
+	fi
+
+	# Remove control files
+	rm "$CONTROLDIR/control_$FILENAME"
+	rm "$CONTROLDIR/commands_$FILENAME"
+
+done
+
+END=`date '+%H:%M:%S'`
+echo "Script exited @ $END with retval: ${script_ret_val}"
+
+exit $script_ret_val
+
+
