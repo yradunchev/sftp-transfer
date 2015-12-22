857345f2615bf578ff4ae672191f892f4559118a
Yordan Radunchev <yordan@radunchev.com>
2015-12-22 10:30:05 +0200

    add function in_use()
    previously this check was done in the script, moved as separate function.

--- general-scripts/transfer_gen_functions.sh
+++ general-scripts/transfer_gen_functions.sh
@@ -11,3 +11,12 @@ check_error() {
     fi
 	return 0
 }
+
+in_use() {
+	lsof -w > /dev/null 2>&1 "${1}"
+	if [ $? -gt 0 ]; then
+		echo "File ${1} is still in use, deferring the transfer for that file."
+		return 1
+	fi
+		return 0
+}
